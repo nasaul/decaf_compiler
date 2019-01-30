28b844308e947ffcb955ae7f59ca751c8c716e41
Saúl Caballero <saul.caballero.ramirez@gmail.com>
2019-01-30 12:29:02 -0600

    Intento un tester

---
+++ PP1/tester.sh
@@ -0,0 +1,17 @@
+
+programas_frag=($(ls samples | grep -oP '.*\.frag' | grep -oP '.*\.'))
+programas_decaf=($(ls samples | grep -oP '.*\.decaf' | grep -oP '.*\.'))
+
+for item in ${programas_frag[@]}; do
+  (./dcc < "samples/"$item"frag") > "samples/"$item"out1" 2>&1
+done
+
+for item in ${programas_decaf[@]}; do
+  (./dcc < "samples/"$item"decaf") > "samples/"$item"out1" 2>&1
+done
+
+programas=($(ls samples | grep -oP '.*\.' | uniq))
+
+for item in ${programas[@]}; do
+  diff --text "samples/"$item"out" "samples/"$item"out1"
+done
