0fe3364ac18e038ab0d028d5d5a7f9b572799f4f
Saúl Caballero <saul.caballero.ramirez@gmail.com>
2019-05-04 18:59:02 -0500

    Declaraciones

--- PP4/ast_decl.h
+++ PP4/ast_decl.h
@@ -4,51 +4,81 @@
  * manage declarations. There are 4 subclasses of the base class,
  * specialized for declarations of variables, functions, classes,
  * and interfaces.
+ *
+ * pp3: You will need to extend the Decl classes to implement 
+ * semantic processing including detection of declaration conflicts 
+ * and managing scoping issues.
  */
 
 #ifndef _H_ast_decl
 #define _H_ast_decl
 
 #include "ast.h"
+#include "ast_stmt.h"
+#include "ast_type.h"
 #include "list.h"
 
 class Type;
 class NamedType;
 class Identifier;
 class Stmt;
-class FnDecl;
-class InterfaceDecl;
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/// Decl
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 class Decl : public Node 
 {
   protected:
     Identifier *id;
+    Scope *scope;
   
   public:
     Decl(Identifier *name);
     friend std::ostream& operator<<(std::ostream& out, Decl *d) { return out << d->id; }
-    Identifier *GetId() { return id; }
-    const char *GetName() { return id->GetName(); }
     
-    virtual bool ConflictsWithPrevious(Decl *prev);
+    virtual bool AreEquiv(Decl *other);
 
-    virtual bool IsVarDecl() { return false; } // jdz: could use typeid/dynamic_cast for these
-    virtual bool IsClassDecl() { return false; }
-    virtual bool IsInterfaceDecl() { return false; }
-    virtual bool IsFnDecl() { return false; } 
-    virtual bool IsMethodDecl() { return false; }
+    const char* Name() { return id->Name(); }
+    Scope* GetScope() { return scope; }
+
+    virtual void ScopeMake(Scope *parent);
+    virtual void Check() = 0;
 };
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/// VarDecl
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 class VarDecl : public Decl 
 {
   protected:
     Type *type;
+    //bool type_declared;
     
   public:
     VarDecl(Identifier *name, Type *type);
+    
+    bool AreEquiv(Decl *other);
+
+    Type* TypeFinder() { return type; }
     void Check();
-    Type *GetDeclaredType() { return type; }
+    //bool is_declared_type() { return type_declared; }
+    
+    
+  private:
+    void FindType();
 };
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/// ClassDecl
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 class ClassDecl : public Decl 
 {
@@ -56,16 +86,30 @@ class ClassDecl : public Decl
     List<Decl*> *members;
     NamedType *extends;
     List<NamedType*> *implements;
-    Type *cType;
-    List<InterfaceDecl*> *convImp;
 
   public:
     ClassDecl(Identifier *name, NamedType *extends, 
               List<NamedType*> *implements, List<Decl*> *members);
+    void ScopeMake(Scope *parent);
     void Check();
-    bool IsClassDecl() { return true; }
-    Scope *PrepareScope();
+    NamedType* TypeFinder() { return new NamedType(id); }
+    NamedType* GetExtends() { return extends; }
+    List<NamedType*>* GetImplements() { return implements; }
+
+  private:
+    void ExtendsFinder();
+    void ImplementsFinder();
+    void ExtendedMembersFinder(NamedType *extType);
+    void ImplementedMembersFinder(NamedType *impType);
+    void AgScopeFinder(Scope *other);
+    void ImplementsFinderInterfaces();
 };
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/// InterfaceDecl
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 class InterfaceDecl : public Decl 
 {
@@ -74,10 +118,17 @@ class InterfaceDecl : public Decl
     
   public:
     InterfaceDecl(Identifier *name, List<Decl*> *members);
+    void ScopeMake(Scope *parent);
     void Check();
-    bool IsInterfaceDecl() { return true; }
-    Scope *PrepareScope();
+    Type* TypeFinder() { return new NamedType(id); }
+    List<Decl*>* GetMembers() { return members; }
 };
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/// FnDecl
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 class FnDecl : public Decl 
 {
@@ -89,11 +140,11 @@ class FnDecl : public Decl
   public:
     FnDecl(Identifier *name, Type *returnType, List<VarDecl*> *formals);
     void SetFunctionBody(Stmt *b);
+    bool AreEquiv(Decl *other);
+    Type* GetReturnType() { return returnType; }
+    List<VarDecl*>* GetFormals() { return formals; }
+    void ScopeMake(Scope *parent);
     void Check();
-    bool IsFnDecl() { return true; }
-    bool IsMethodDecl();
-    bool ConflictsWithPrevious(Decl *prev);
-    bool MatchesPrototype(FnDecl *other);
 };
 
 #endif
