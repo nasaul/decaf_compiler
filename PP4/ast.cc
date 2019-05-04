605f1a1cf02b365f2543058d74f44c2762571d83
Saúl Caballero <saul.caballero.ramirez@gmail.com>
2019-05-04 18:53:30 -0500

    agrego ast

--- PP4/ast.cc
+++ PP4/ast.cc
@@ -7,33 +7,21 @@
 #include "ast_decl.h"
 #include <string.h> // strdup
 #include <stdio.h>  // printf
-#include "errors.h"
-#include "scope.h"
 
 Node::Node(yyltype loc) {
     location = new yyltype(loc);
     parent = NULL;
-    nodeScope = NULL;
 }
 
 Node::Node() {
     location = NULL;
     parent = NULL;
-    nodeScope = NULL;
-}
-
-Decl *Node::FindDecl(Identifier *idToFind, lookup l) {
-    Decl *mine;
-    if (!nodeScope) PrepareScope();
-    if (nodeScope && (mine = nodeScope->Lookup(idToFind)))
-        return mine;
-    if (l == kDeep && parent)
-        return parent->FindDecl(idToFind, l);
-    return NULL;
 }
 	 
 Identifier::Identifier(yyltype loc, const char *n) : Node(loc) {
     name = strdup(n);
-    cached = NULL;
 } 
 
+bool Identifier::operator==(const Identifier &rhs) {
+    return strcmp(name, rhs.name) == 0 ? true : false;
+}
